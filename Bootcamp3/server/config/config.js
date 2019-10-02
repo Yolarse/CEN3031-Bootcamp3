a17ded4d75157e046c758903a0b1ed8a30f4cb6f
Michael Lee <michael.lee@ufl.edu>
2019-10-02 03:42:22 -0400

    Delete config.js

--- Bootcamp3/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://michael_lee:p2TPBXOQJGSh858b@cluster0-tivui.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-  },
-  openCage: {
-    key: '8b37e627fddc411eaf338c575ce91bcb' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
-  },
-  port: 8080
-};
